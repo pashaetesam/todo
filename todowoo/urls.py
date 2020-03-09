37b8d4e79c1a058f6ca076f89b91af5ad8d48fb0
pashaetesam <pauletesam@comcast.net>
2020-03-09 06:23:27 -0700

    first commit

---
+++ todowoo/urls.py
@@ -0,0 +1,19 @@
+
+from django.contrib import admin
+from django.urls import path
+from todo import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+# Auth
+    path('signup/',views.signupuser,name='signupuser'),
+    path('logout/',views.logoutuser,name='logoutuser'),
+    path('login/',views.loginuser,name='loginuser'),
+
+# Todo
+     path('',views.home,name='home'),
+     path('current/',views.currenttodos,name='currenttodos'),
+
+
+]
